dddc13385d80f5dbb5090014d46c019fe3673afa
Kutejiang <124875260+kutejian@users.noreply.github.com>
2023-03-29 22:51:21 +0800

    Add files via upload

---
+++ AutofacAndJwt/WebWithAutofacAndJwt/Model/UserRoles.cs
@@ -0,0 +1,7 @@
+﻿namespace WebWithAutofacAndJwt.Model;
+public static class UserRoles
+{
+    public const string Admin = nameof(Admin);
+    public const string BasicUser = nameof(BasicUser);
+}
+
---
+++ AutofacAndJwt/WebWithAutofacAndJwt/Service/IUserService.cs
@@ -0,0 +1,7 @@
+﻿namespace WebWithAutofacAndJwt.Service;
+public interface IUserService
+{
+    bool IsAnExistingUser(string userName);
+    bool IsValidUserCredentials(string userName, string password);
+    string GetUserRole(string userName);
+}
